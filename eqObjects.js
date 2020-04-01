ac116567e085d1103ee24b4902dd5eafc8af5ce8
Joey Kishiuchi <your@email.comcd>
2020-04-01 23:24:44 +0000

    function updated to compare objects with values of primitive data and arrays. Still needs to incorporate comparing values that are objects -wip

--- eqObjects.js
+++ eqObjects.js
@@ -6,12 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqObjects = function(object1, object2) {
-  const object1Keys = Object.keys(object1);
-  const object2Keys = Object.keys(object2);
-  if (object1Keys.length === object2Keys.length) {
-    for (let key of object1Keys) {
-      if (object1[key] !== object2[key]) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
@@ -21,9 +19,28 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-const ab = { a: "1", b: "2", c: "3"};
-const ba = { b: "2", a: "1", c: "3"};
-assertEqual(eqObjects(ab, ba), true); // => true
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length === object2Keys.length) {
+    for (let key of object1Keys) {
+      if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      } else if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const abc = { a: "1", b: "2", c: "4" };
-assertEqual(eqObjects(ab, abc), false); // => false
+const cd = { d: "1", c: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), false); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
