c7af48a5dc4252a06a60dd52d888a43acbeb6797
Joey Kishiuchi <your@email.comcd>
2020-04-01 22:03:44 +0000

    completed letterPositions function including tests

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+  return (JSON.stringify(arr1) === JSON.stringify(arr2));
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+const result1 = letterPositions('hello');
+assertArraysEqual(result1.h, [0]);
+assertArraysEqual(result1.e, [1]);
+assertArraysEqual(result1.l, [2, 3]);
+assertArraysEqual(result1.o, [4]);
