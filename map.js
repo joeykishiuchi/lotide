8c6be2050558df89fde4c96e2a2954146b91b2d6
Joey Kishiuchi <your@email.comcd>
2020-04-02 21:47:54 +0000

    added another assertion test

--- map.js
+++ map.js
@@ -10,18 +10,27 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
 
 const results1 = map(words, word => word[0]);
 assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
 
-const results2 = map(words, word => word.length);
-assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+const results2 = map(words, word => word + '!');
+assertArraysEqual(results2, ["ground!", "control!", "to!", "major!", "tom!"]);
+
+const results3 = map(words, word => {
+  if (word === 'tom') {
+    return 'jim';
+  }
+  return word;
+});
+assertArraysEqual(results3, ["ground", "control", "to", "major", "jim"]);
+
