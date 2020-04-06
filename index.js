f8e1205900010b647ec5cf7cd93fdff79746f973
Joey Kishiuchi <your@email.comcd>
2020-04-06 21:25:51 +0000

    importing all applicable functions from lotide to index.js to be easily exported as a single object

---
+++ index.js
@@ -0,0 +1,27 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const flatten = require('./flatten');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  flatten,
+  findKey,
+  findKeyByValue,
+  countOnly,
+  countLetters,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
