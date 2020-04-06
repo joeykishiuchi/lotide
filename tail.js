de40be3d3bd4ee3859ec880bf97bd18cb19107c0
Joey Kishiuchi <your@email.comcd>
2020-04-06 20:44:08 +0000

    removed assertEqual from original files

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let tailArr = array.slice(1,array.length);
     return tailArr;
